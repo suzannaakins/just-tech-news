543c4f63d208756a03446cf3edd157c22ccd4826
Garlic Fiend <suzannaakins@gmail.com>
2020-10-26 15:50:59 -0700

    connection.js file debugged

--- config/connection.js
+++ config/connection.js
@@ -4,9 +4,9 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 //create connection to our database
-const sequelize = new Sequelize(process.env.just_tech_news_db, process.env.DB_USER, process.env.DB_PW, {
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
     host: 'localhost',
-    dialect: 'mysql', 
+    dialect: 'mysql',
     port: 3306
 });
 
