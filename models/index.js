caeca5bccec37bd39ddd2dbd756deee2d1c033d2
Garlic Fiend <suzannaakins@gmail.com>
2020-10-27 12:39:37 -0700

    create a comment

--- models/index.js
+++ models/index.js
@@ -3,6 +3,7 @@
 const User = require('./User');
 const Post = require('./Post');
 const Vote = require('./Vote');
+const Comment = require('./Comment');
 
 //create associations between models
 
@@ -50,4 +51,21 @@ Post.hasMany(Vote, {
     foreignKey: 'post_id'
 });
 
-module.exports = { User, Post, Vote };
+//model associations for COMMENT model
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id'
+});
+
+Post.hasMany(Comment, {
+    foreignKey: 'post_id'
+});
+
+module.exports = { User, Post, Vote, Comment };
