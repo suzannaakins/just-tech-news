d9d48da0c755566b83cc89e2b52c5067a35c8abf
Garlic Fiend <suzannaakins@gmail.com>
2020-10-26 22:12:44 -0700

    delete a post

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -89,4 +89,24 @@ router.put('/:id', (req, res) => {
         });
 });
 
+router.delete('/:id', (req, res) => {
+    Post.destroy(
+        {
+            where: {
+                id: req.params.id
+            }
+        })
+        .then(dbPostData => {
+            if (!dbPostData) {
+                res.status(404).json({ message: 'No post with this id' });
+                return;
+            }
+            res.json(dbPostData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
 module.exports = router;
