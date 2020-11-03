5ae4d10d21483346c8281d10b670ae9b8e9140f0
Garlic Fiend <suzannaakins@gmail.com>
2020-11-02 20:32:58 -0800

    logged-in users can comment on posts

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -28,16 +28,19 @@ router.get('/', (req, res) => {
 
 //create a comment
 router.post('/', (req, res) => {
-    Comment.create({
-        comment_text: req.body.comment_text,
-        user_id: req.body.user_id,
-        post_id: req.body.post_id
-    })
-        .then(dbCommentData => res.json(dbCommentData))
-        .catch(err => {
-            console.log(err);
-            res.status(400).json(err);
-        });
+    //make sure user logged in (session) 
+    if (req.session) {
+        Comment.create({
+            comment_text: req.body.comment_text,
+            user_id: req.session.user_id,
+            post_id: req.body.post_id
+        })
+            .then(dbCommentData => res.json(dbCommentData))
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            });
+    }
 });
 
 //delete a comment
