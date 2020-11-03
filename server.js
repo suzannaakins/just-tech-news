9a245df542c47012161dcfdb593dffcda1330a64
Garlic Fiend <suzannaakins@gmail.com>
2020-11-02 22:17:28 -0800

    date helper implemented in template

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const routes = require('./controllers/');
 const sequelize = require('./config/connection');
 const path = require('path');
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
+const helpers = require('./utils/helpers');
+const hbs = exphbs.create({ helpers });
 const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const sess = {
@@ -15,7 +16,6 @@ const sess = {
         db: sequelize
     })
 };
-
 const app = express();
 const PORT = process.env.PORT || 3001;
 
