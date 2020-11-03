f377859afc4587c717de8138d6b16533811afda8
Garlic Fiend <suzannaakins@gmail.com>
2020-11-02 17:40:13 -0800

    destroy session to allow user to log out

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -110,6 +110,17 @@ router.post('/login', (req, res) => {
         });
 });
 
+//allow users to log OUT
+router.post('/logout', (req, res) => {
+    if (req.session.loggedIn) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    } else {
+        res.status(404).end();
+    }
+});
+
 //PUT /api/users/1  --> UPDATES user
 router.put('/:id', (req, res) => {
     //expects username, email, password
