3d7057ea47220edfd9ccfdc9269c2166dc1c7cf5
Garlic Fiend <suzannaakins@gmail.com>
2020-11-02 22:36:10 -0800

    dashboard link on main page, and logged in users redirected to it

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -41,7 +41,7 @@ async function loginFormHandler(event) {
         });
         //check response status
         if (response.ok) {
-            document.location.replace('/');
+            document.location.replace('/dashboard');
         } else {
             alert(response.statusText);
         }
